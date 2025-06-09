651f9333b290122439e4b0cb5559cc60f5c1a09d
anacarlaam <anacarlaam@gmail.com>
2025-06-09 00:58:46 -0300

    Meu terceiro  progrma do bootcamp

---
+++ exemplo03.py
@@ -0,0 +1,7 @@
+# refatore o exercicio 02 usando variáveis
+# Digite dois numeros e retorne a soma
+
+numero1 = int(input("Digite o 1o numero: "))
+numero2 = int(input("Digite o 2o numero: "))
+soma = numero1 + numero2
+print ("A soma é: ", soma)
